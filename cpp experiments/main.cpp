2f43c9fca267150a6287b67059525f00e9a988b0
Grzegorz Koperwas <grzes869@gmail.com>
2019-10-23 08:11:36 +0200

    sync

--- cpp experiments/main.cpp
+++ cpp experiments/main.cpp
@@ -2,8 +2,8 @@
 #include <string>
 using namespace std;
 
-long silnia(long num){
-	long res;
+int silnia(int num){
+	int res;
 	if(num == 0){
 		res = 1;
 	}
@@ -16,28 +16,42 @@ long silnia(long num){
 
 	return res;
 }
-void prlongNum(long num){
-	string str = to_string(num);
-	if(num > 0){
-		long polong = str.size() - 1;
+
+int getLast(int num){
+	if(num == 0){
+		return 0;
+	}
+	else{
 		while(true){
-			if(str.at(polong) == '0'){
-				polong--;	
+			int x = num % 10;
+			if(x == 0){
+				num = num/10;
 			}
 			else{
+				return x;
 				break;
 			}
 		}
-		cout << str.at(polong) << "\n";
 	}
 }
 
+int impSilnia(int num){
+	int acc = 1;
+	for(int x = 1; x < num; x++){
+		acc = getLast( acc * getLast(num));
+	}
+	return acc;
+}
+
+bool tester(int num){
+	return getLast(silnia(num)) == impSilnia(num); 
+}
 
 int main(){
-	for(int i = 1; i < 95; i++){
-		long duze = silnia(i);
-		cout << "basenum: " << duze << "\t";
-		prlongNum(duze);
+	for(int i = 1; i < 20; i++){
+		int duze = impSilnia(i);
+		cout << "basenum: " << i << "\t" << "duze: " << duze << "\n";
+		//cout << "test " << i << "\t" << tester(i) << "\n"; 
 	}
 	return 0;
 }
