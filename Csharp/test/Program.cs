feda5d5dbaba4af167b9e74d97eb01943f4f50d8
Grzegorz Koperwas <grzes869@gmail.com>
2020-02-23 17:36:19 +0100

    done

--- Csharp/test/Program.cs
+++ Csharp/test/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace test
+//To jest pierwszy program jaki napisałem w C# poza Unity
+//Testowany na mono w Manjaro Linux
 {
     class Program
     {
@@ -16,26 +18,55 @@ namespace test
               "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm",
               "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg",
               "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"};
-        static List<List> mendalejew(string toChange)
+        static List<string> mendalejew(string toChange)
         {
+            List<string> res = new List<string>();
             int i = 0;
+            //Sprawdz czy coś pasuje, jak tak to dopisz to res
             foreach (string x in pierwiastki) {
                 i++;
                 string pierwiastek = x.ToLower();
+                if (toChange == pierwiastek){
+                  res.Add(i.ToString());
+                  //res.Add(x);
+                }
                 if (toChange.StartsWith(pierwiastek)){
-                    Console.WriteLine(i);
-                    mendalejew(toChange.Substring(pierwiastek.Length));
+                    //Sprawdz czy do tego co zostało coś pasuje
+                    List<string> results = mendalejew(toChange.Substring(pierwiastek.Length));
+                    foreach (string I in results){
+                        res.Add(i + "*" + I);
+                        //res.Add(x + I);
+                    }
                 }
             }
-            string X = "xd";
-            return X;
+            return res;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            string fraza = "ziemniaki";
-            mendalejew(fraza);
+            string dane = Console.ReadLine();
+            string[] frazy = dane.Split(" ");
+            List<List<string>> results = new List<List<string>>();
+            foreach (string fraza in frazy){
+                List<string> x = mendalejew(fraza.ToLower());
+                results.Add(x);
+            }
+            string display = "";
+            bool sukces = true;
+            foreach (List<string> word in results){
+                if (word.Count == 0) {
+                  Console.WriteLine("Nie można zapisać frazy szyfrem mendelejewa, odkryj więcej pierwiastków");
+                  sukces = false;
+                }
+                else{
+                  display = display + "**" + word[0];
+                }
+            }
+            if (sukces) {
+              Console.WriteLine(display.Substring(2));
+            }
+
+
         }
     }
 }
