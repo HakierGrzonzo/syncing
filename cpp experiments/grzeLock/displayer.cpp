c8369e8b2f12742de95f5fbe7546a862cbae36a7
Grzegorz Koperwas <grzes869@gmail.com>
2020-11-02 20:12:55 +0100

    displaysSth

--- cpp experiments/grzeLock/displayer.cpp
+++ cpp experiments/grzeLock/displayer.cpp
@@ -1,7 +1,11 @@
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Window/WindowStyle.hpp>
 #include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/Sprite.hpp>
+#include <time.h>
+#include <stdio.h>
 
 struct res
 {
@@ -9,21 +13,58 @@ struct res
     unsigned int y;
 };
 
+// Get current date/time, format is HH:MM
+const std::string currentDateTime() {
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+    // for more information about date/time format
+    strftime(buf, sizeof(buf), "%H:%M", &tstruct);
+
+    return buf;
+}
+
 void lockScreen(res screen)
 {
-    sf::RenderWindow window(sf::VideoMode(screen.x, screen.y), "grzeLock");
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 2;
+
+    sf::RenderWindow window(sf::VideoMode(screen.x, screen.y), "grzeLock", sf::Style::Close | sf::Style::Fullscreen, settings);
+    window.setVerticalSyncEnabled(true);
+    
+    // Handle background
 
     sf::Texture background;
     if (!background.loadFromFile("background.png"))
     {
-        // on failure create black image
+        // on failure create something
         background.create(screen.x, screen.y);
     }
+    background.setSmooth(true);
 
-    sf::Sprite backgroundSprite;
-    backgroundSprite.setTexture(background);
+    sf::RectangleShape backgroundShape(sf::Vector2f(screen.x, screen.y));
+    backgroundShape.setTexture(&background);
 
-    sf::Vector2u WindowSize = window.getSize();             //Get size of window.
+
+    // handle clock font
+
+    sf::Font clockFont;
+    if (!clockFont.loadFromFile("futura light bt.ttf"))
+    {
+        std::cout << "I can not proceed without clock font, Exiting!" << std::endl;
+        return;
+    }
+
+    sf::Text clock;
+    clock.setFont(clockFont);
+    clock.setCharacterSize(100);
+    clock.setOrigin(sf::Vector2f(1, 0));
+    clock.setPosition(sf::Vector2f((float) 0.1 * screen.x, (float) 0.8 * screen.y ));
+    clock.setFillColor(sf::Color::White);
+
+    std::string time = "";
 
     while (window.isOpen())
     {
@@ -36,16 +77,24 @@ void lockScreen(res screen)
                 std::cout << "Closing window, goodbye!" << std::endl;
                 break;
             }
+            else if (event.type == sf::Event::EventType::Resized)
+            {
+                auto windowSize = window.getSize();
+                //backgroundShape.setSize(sf::Vector2f((float) windowSize.x, (float) windowSize.y));
+                std::cout << "Resized Now to " << windowSize.x << "x" << windowSize.y << std::endl;
+            }
         }
-
+        std::string newTime = currentDateTime();
+        if (time != newTime)
+        {
+            time = newTime;
+            clock.setString(time);
+        }
+        window.clear(); 
         // draw stuff
-        // calculate scale
-        sf::Vector2u TextureSize = background.getSize(); //Get size of texture.
-
-        float ScaleX = (float) WindowSize.x / TextureSize.x;
-        float ScaleY = (float) WindowSize.y / TextureSize.y;     //Calculate scale.
-        backgroundSprite.setScale(sf::Vector2f(ScaleX, ScaleY));
-        window.draw(backgroundSprite);
+        // Draw Background sprite
+        window.draw(backgroundShape);
+        window.draw(clock);
         window.display();
     }
 }
