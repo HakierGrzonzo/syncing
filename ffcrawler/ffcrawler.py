f4c667e6f55f6142801bf60d2523b17d8e7ad540
Grzegorz Koperwas <grzes869@gmail.com>
2020-11-15 18:46:29 +0100

    added cache

--- ffcrawler/ffcrawler.py
+++ ffcrawler/ffcrawler.py
@@ -50,54 +50,64 @@ print("\nWaiting for RateLimit")
 print("Skipped {} works".format(skipped))
 pWait(60)
 print("Processing works!")
-
 results = list()
+cached = list()
+try:
+    with open("result.json", "r") as f:
+        results = json.load(f)
+        cached = list([x["url"] for x in results])
+        print("Continuing from file")
+except Exception as e:
+    print(e)
+
 
 try:
     for workid in works:
-        this_rate_limit = 3
-        id = AO3.utils.workid_from_url(workid.url)
         print("\rProcessing work {}/{}".format(len(results) + 1, len(works)), end="")
-        while True:
-            try:
-                work = AO3.Work(id)
-                res = {
-                    "title" : work.title,
-                    "hits" : work.hits,
-                    "kudos" : work.kudos,
-                    "language" : work.language,
-                    "url" : work.url,
-                    "chapters" : work.chapters,
-                    "words" : work.words,
-                    "rating": work.rating,
-                    "categories" : work.categories,
-                    "characters" : work.characters,
-                    "tags" : work.tags,
-                    "date_updated" : work.date_updated.isoformat(),
-                    "authors": [
-                        {
-                            "username" : author.username,
-                            "url" : author.url
-                        }
-                        for author in work.authors
-                    ],
-                    "relationships" : work.relationships
-                }
-                results.append(res)
-                break
-            except AO3.utils.HTTPError as e:
-                this_rate_limit += 1
-                timer = this_rate_limit * 30
-                if this_rate_limit > 5:
-                    print("\nSkiping this fic!")
-                    rateLimitedCount -= this_rate_limit
+        if workid.url not in cached:
+            this_rate_limit = 3
+            id = AO3.utils.workid_from_url(workid.url)
+            while True:
+                try:
+                    work = AO3.Work(id)
+                    res = {
+                        "title" : work.title,
+                        "hits" : work.hits,
+                        "kudos" : work.kudos,
+                        "language" : work.language,
+                        "url" : work.url,
+                        "chapters" : work.chapters,
+                        "words" : work.words,
+                        "rating": work.rating,
+                        "categories" : work.categories,
+                        "characters" : work.characters,
+                        "tags" : work.tags,
+                        "date_updated" : work.date_updated.isoformat(),
+                        "authors": [
+                            {
+                                "username" : author.username,
+                                "url" : author.url
+                            }
+                            for author in work.authors
+                        ],
+                        "relationships" : work.relationships
+                    }
+                    results.append(res)
                     break
-                print("\nRatelimit detected! trying again in {} seconds".format(timer))
-                pWait(timer)
-                continue
+                except AO3.utils.HTTPError as e:
+                    this_rate_limit += 1
+                    timer = this_rate_limit * 30
+                    if this_rate_limit > 5:
+                        print("\nSkiping this fic!")
+                        rateLimitedCount -= this_rate_limit
+                        break
+                    print("\nRatelimit detected! trying again in {} seconds".format(timer))
+                    pWait(timer)
+                    continue
     if len(results) % 100 == 0:
         with open("result.json", "w+") as f:
             f.write(json.dumps(results, indent= 4))
+            print("\nWriting results to file")
 except KeyboardInterrupt:
     print("\nrecived interrupt, proceeding with {} works..".format(len(results)))
 
